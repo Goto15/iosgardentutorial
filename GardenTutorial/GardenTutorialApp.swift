e1221c7ae973db98381c491bdb92d8ef906d54f0
GoTo15 <goto15pod@gmail.com>
2021-09-28 21:02:53 -0500

    Initial Commit

---
+++ GardenTutorial/GardenTutorialApp.swift
@@ -0,0 +1,20 @@
+//
+//  GardenTutorialApp.swift
+//  GardenTutorial
+//
+//  Created by Ben Sullivan on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct GardenTutorialApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
